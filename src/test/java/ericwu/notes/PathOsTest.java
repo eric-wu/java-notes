e3fb7d5562c107f566a17f00c7d3c00061d37ec8
Eric Wu <ericwu@ge.com>
2015-12-02 06:56:30 -0800

    Better documentation.

--- src/test/java/ericwu/notes/PathOsTest.java
+++ src/test/java/ericwu/notes/PathOsTest.java
@@ -4,32 +4,38 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.apache.commons.lang3.SystemUtils;
 import org.junit.Test;
 
 /**
- * Tests Path behavior different on different operating systems.
+ * Tests that Path behavior can be different on different operating systems.
  */
 public class PathOsTest {
 
     @Test
-    public void testGetSpaceAsPathWindows() {
-        if ( SystemUtils.IS_OS_WINDOWS ) {
+    public void testGetSpaceAsPathOnWindows() {
+        final String msg = "Expected InvalidPathException on Windows.";
+        if (SystemUtils.IS_OS_WINDOWS) {
             try {
-                Paths.get( " " );
-                fail();
-            } catch ( InvalidPathException e ) {
-                assertTrue( "Expected exception", true );
+                getSpaceAsPath();
+                fail(msg);
+            } catch (InvalidPathException e) {
+                assertTrue(msg, true);
             }
         }
     }
 
     @Test
-    public void testGetSpaceAsPathLinux() {
-        if ( SystemUtils.IS_OS_LINUX ) {
-            Paths.get( " " );
+    public void testGetSpaceAsPathOnLinux() {
+        if (SystemUtils.IS_OS_LINUX) {
+            getSpaceAsPath();
         }
+    }
+
+    private Path getSpaceAsPath() {
+        return Paths.get(" ");
     }
 }
